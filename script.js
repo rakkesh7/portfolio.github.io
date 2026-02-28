5b7a1cea50d4353e70fb77b5cbce111902df568c
rakkesh <138961462+rakkesh7@users.noreply.github.com>
2026-03-01 04:53:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,26 @@
-let menuIcon = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
-let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a');
+let menuIcon=document.querySelector('#menu-icon');
+let navbar=document.querySelector('.navbar');
+let section=document.querySelectorAll('section');
+let navlinks=document.querySelectorAll('header nav a');
 
-/* ===== MENU TOGGLE ===== */
-menuIcon.onclick = () => {
-  menuIcon.classList.toggle('bx-x');
-  navbar.classList.toggle('active');
-};
-
-/* ===== ACTIVE LINK ON SCROLL ===== */
 window.onscroll = () => {
-  sections.forEach(sec => {
-    let top = window.scrollY;
-    let offset = sec.offsetTop - 150;
-    let height = sec.offsetHeight;
-    let id = sec.getAttribute('id');
+section.forEach(sec => {
+let top = window.scrollY;
+let offset = sec.offsetTop - 150;
+let height = sec.offsetHeight;
+let id= sec.getAttribute('id');
 
-    if (top >= offset && top < offset + height) {
-      navLinks.forEach(link => {
-        link.classList.remove('active');
-        document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
-      });
-    }
-  });
-};
+if (top>= offset && top< offset+height){  
+    navlinks.forEach(links=>{  
+        links.classList.remove('active');  
+        document.querySelector('header nav a[href*='+ id+']').classList.add('active')  
+    })  
+ }  
+})
+
+}
+
+menuIcon.onclick = () =>{
+menuIcon.classList.toggle('bx-x');
+navbar.classList.toggle('active');
+}
